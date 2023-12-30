f02ab43562c8cc6c72ba02714f4e6cd4352b353c
marcowang01 <marcowang01@gmail.com>
2023-12-30 11:17:08 +0800

    change title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Hellsegga',
+  title: 'Hellsegga - A Creative Technologist Studio',
   description: 'A Creative Technologist Studio',
 }
 
